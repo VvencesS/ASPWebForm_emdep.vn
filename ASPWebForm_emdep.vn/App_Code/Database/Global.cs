0c3e2203c4613d523d575146fde0064f51a80d1d
= <=>
2020-05-27 01:06:31 +0700

    Build pages to add new product categories.

---
+++ ASPWebForm_emdep.vn/App_Code/Database/Global.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPWebForm_emdep.vn.App_Code.Database
+{
+    public class Global
+    {
+    }
+}
--- ASPWebForm_emdep.vn/cms/admin/SanPham/QuanLyDanhMuc/DanhMuc_ThemMoi.ascx.cs
+++ ASPWebForm_emdep.vn/cms/admin/SanPham/QuanLyDanhMuc/DanhMuc_ThemMoi.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,65 @@ namespace ASPWebForm_emdep.vn.cms.admin.SanPham.QuanLyDanhMuc
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LayDanhMucCha();
+            }
+        }
+        private void LayDanhMucCha()
+        {
+            DataTable dt = new DataTable();
+            dt = ASPWebForm_emdep.vn.App_Code.Database.DanhMuc.Thongtin_Danhmuc_by_MaDMCha("0");
+            ddlDanhMucCha.Items.Clear();
 
+            ddlDanhMucCha.Items.Add(new ListItem("Danh mục gốc", "0"));
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ddlDanhMucCha.Items.Add(new ListItem(dt.Rows[i]["TenDM"].ToString(), dt.Rows[i]["MaDM"].ToString()));
+                //LayDanhMucCon(dt.Rows[i]["MaDM"].ToString(), "___");
+            }
+        }
+        protected void btThemMoi_Click(object sender, EventArgs e)
+        {
+            #region code nút thêm mới
+
+            if (flAnhDaiDien.FileContent.Length > 0)
+            {
+                if (flAnhDaiDien.FileName.EndsWith(".jpeg") || flAnhDaiDien.FileName.EndsWith(".jpg") || flAnhDaiDien.FileName.EndsWith(".png") || flAnhDaiDien.FileName.EndsWith(".gif"))
+                {
+                    flAnhDaiDien.SaveAs(Server.MapPath("pic/SanPham/") + flAnhDaiDien.FileName);
+                }
+            }
+            //else ltrThongBao.Text = "Nhập sai hoặc thiếu thông tin! Mời nhập lại";
+
+
+            ASPWebForm_emdep.vn.App_Code.Database.DanhMuc.Danhmuc_Inser(tbTenDanhMuc.Text, flAnhDaiDien.FileName, tbThuTu.Text, ddlDanhMucCha.SelectedValue, "");
+            ltrThongBao.Text = "<div class='thongBaoTaoThanhCong' style='color:#ff006e;font-size:16px;padding-bottom:20px;text-align:center;font-weight:bold'>Đã tạo danh mục: " + tbTenDanhMuc.Text + "</div>";
+
+            if (cbThemNhieuDanhMuc.Checked)
+            {
+                //viết code xử lý xóa mục đã nhập để người dùng nhập danh mục tiếp theo
+                ResetControl();
+            }
+
+            else
+            {
+                //đẩy trang về trang danh sách các damnh mục đã tạo
+
+                Response.Redirect("Admin.aspx?modul=SanPham&modulphu=DanhMuc");
+            }
+            #endregion
+        }
+
+        private void ResetControl()
+        {
+            tbTenDanhMuc.Text = "";
+            tbThuTu.Text = "";
+
+        }
+        protected void btHuy_Click(object sender, EventArgs e)
+        {
+            ResetControl();
         }
     }
 }
