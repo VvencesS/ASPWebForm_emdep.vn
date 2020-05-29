b0b370d4e13e1fbf48da81f86c61d7c2a2fcdcb0
= <=>
2020-05-30 01:14:13 +0700

    Build function to add / edit / delete product groups.

---
+++ ASPWebForm_emdep.vn/cms/admin/SanPham/QuanLyNhomSanPham/Ajax/NhomSanPham.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NhomSanPham.aspx.cs" Inherits="ASPWebForm_emdep.vn.cms.admin.SanPham.QuanLyNhomSanPham.Ajax.NhomSanPham" %>
+
--- ASPWebForm_emdep.vn/cms/admin/SanPham/QuanLyNhomSanPham/NhomSanPham_HienThi.ascx.cs
+++ ASPWebForm_emdep.vn/cms/admin/SanPham/QuanLyNhomSanPham/NhomSanPham_HienThi.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,34 @@ namespace ASPWebForm_emdep.vn.cms.admin.SanPham.QuanLyNhomSanPham
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                LayNhomSanPham();
+        }
+
+        private void LayNhomSanPham()
+        {
+            DataTable dt = new DataTable();
+            dt = ASPWebForm_emdep.vn.App_Code.Database.NhomSanPham.Thongtin_Nhomsp();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ltrNhomSanPham.Text += @"
+                <tr id='maDong_" + dt.Rows[i]["NhomID"] + @"'>
+                    <td class='cotMa'>" + dt.Rows[i]["NhomID"] + @"</td>
+                    <td class='cotTen'>" + dt.Rows[i]["TenNhom"] + @"</td>
+                    <td class='cotAnh'>
+                        <img class='anhDaiDien'src='/pic/SanPham/" + dt.Rows[i]["AnhDaiDien"] + @"'/>
+                        <img class='anhDaiDienHover'src='/pic/SanPham/" + dt.Rows[i]["AnhDaiDien"] + @"'/>
+                    </td>
+                    <td class='cotThuTu'>" + dt.Rows[i]["ThuTu"] + @"</td>
+                    <td class='cotSoSanPhamHienThi'>" + dt.Rows[i]["SoSPHienThi"] + @"</td>
+                    <td class='cotCongCu'>
+              
+                        <a href='Admin.aspx?modul=SanPham&modulphu=NhomSanPham&thaotac=ChinhSua&id=" + dt.Rows[i]["NhomID"] + @"' class='sua' title='Sửa'></a>
+                        <a href='javascript:XoaNhomSanPham(" + dt.Rows[i]["NhomID"] + @")' class='xoa' title='Xóa'></a>
+                    </td>
+                </tr>
+                ";
+            }
 
         }
     }
